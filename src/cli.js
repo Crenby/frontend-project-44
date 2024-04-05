ddb9b180ed5470402218a37222501cbec0de1aa3
Crenby <arkadiy.shtolts@mail.ru>
2024-04-05 14:17:32 +0300

    4 Шаг

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 function nameQuestion() {
     const name = readlineSync.question('May I have your name?');
     console.log("Hello, " + name + "!");
-};
+}
 
 export default nameQuestion;
 
